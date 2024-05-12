84ddc3e45e57d986505edfd0ba612d6173e5bd8a
ryqdev <ykshang4119@gmail.com>
2024-05-13 01:02:59 +0800

    add color in P&H

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ from typing import NoReturn, List
 from loguru import logger
 from dotenv import load_dotenv
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def main() -> NoReturn:
     parser = argparse.ArgumentParser(description='silver')
@@ -46,7 +57,13 @@ def handle_backtest(strategy: str) -> NoReturn:
     clickhouse_client = get_clickhouse_client()
     start_price = read_clickhouse(clickhouse_client, symbol, start_date)[0][4]
     end_price = read_clickhouse(clickhouse_client, symbol, end_date)[0][4]
-    logger.info(f"{holding}: {start_price}, {end_price}")
+    start_port = float(start_price) * int(holding)
+    end_port = float(end_price) * int(holding)
+    p_h = (end_port - start_port) / start_port
+    if p_h >= 0 :
+        logger.info(f"P&H: {bcolors.OKGREEN}{100 * p_h}%")
+    else:
+        logger.info(f"P&H: {bcolors.FAIL}{100 * p_h}%")
 
 
 def handle_data(symbol: str) -> NoReturn:
