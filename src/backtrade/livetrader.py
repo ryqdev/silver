c88258fe0b0615c01de54763342bdce9df0061fd
ryqdev <ykshang4119@gmail.com>
2024-05-16 13:32:33 +0800

    add ibkr

--- src/backtrade/livetrader.py
+++ src/backtrade/livetrader.py
@@ -1,3 +1,5 @@
+import datetime
+
 import backtrader as bt
 from typing import NoReturn
 
@@ -5,11 +7,14 @@ from src.live.foobar import FooBar
 import backtrader_ib_insync as ibnew
 
 
-
 def live_trader() -> NoReturn:
     cerebro = bt.Cerebro()
+
+    start = datetime.datetime(2024, 4, 1)
+    end = datetime.datetime(2024, 5, 15)
+
     storekwargs = dict(
-        host= 'localhost', port=7497,
+        host='localhost', port=7497,
         clientId=None,
         account=None,
         timeoffset=True,
@@ -17,29 +22,32 @@ def live_trader() -> NoReturn:
         notifyall=True,
         _debug=False,
     )
+
     ibstore = ibnew.IBStore(**storekwargs)
+
     broker = ibstore.getbroker()
     cerebro.setbroker(broker)
-
 
     ibdata = ibstore.getdata
 
     datakwargs = dict(
-    timeframe= bt.TimeFrame.Seconds, compression=1,
-    rtbar=False,
-    qcheck=0.5,
-    what=None,
-    backfill_start=True, backfill=True,
-    latethrough=True,
-    tz=None,
-    useRTH = False,
-    hist_tzo = None,
+        timeframe=bt.TimeFrame.TFrame("Minutes"), compression=1,
+        historical=True, fromdate=start, todate=end,
+        rtbar=False,
+        qcheck=0.5,
+        what=None,
+        backfill_start=True, backfill=True,
+        latethrough=True,
+        tz=None,
+        useRTH=False,
+        hist_tzo=None,
     )
 
-    data0 = ibdata(dataname='EUR.USD-CASH-IDEALPRO', **datakwargs)
+    data0 = ibdata(dataname='SPY-STK-SMART-USD', **datakwargs)
 
-    cerebro.resampledata(data0, timeframe=bt.TimeFrame.Seconds, compression=2)
-
+    # cerebro.resampledata(data0, timeframe=bt.TimeFrame.Seconds, compression=10)
+    cerebro.adddata(data0)
 
     cerebro.addstrategy(FooBar)
+
     cerebro.run()
--- src/live/foobar.py
+++ src/live/foobar.py
@@ -2,15 +2,53 @@ import backtrader as bt
 from loguru import logger
 from src.color import bcolors
 
+Status = [
+    'Created', 'Submitted', 'Accepted', 'Partial', 'Completed',
+    'Canceled', 'Expired', 'Margin', 'Rejected',
+]
+
 
 class FooBar(bt.Strategy):
     def __init__(self):
+        self.data_live = False
         self.log(f"initiating {bcolors.OKGREEN}live strategy...")
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
         logger.info('%s, %s' % (dt.isoformat(), txt))
 
+    def notify_data(self, data, status, *args, **kwargs):
+        print('*' * 5, 'DATA NOTIF:', data._getstatusname(status), *args)
+        if status == data.LIVE:
+            self.data_live = True
+
+    def notify_order(self, order):
+        print("OrderStatus", Status[order.status])
+        if order.status == order.Completed:
+            buysell = 'BUY Completed' if order.isbuy() else 'SELL Completed'
+            txt = '{} {}@{}'.format(buysell, order.executed.size,
+                                    order.executed.price)
+            print(txt)
+
     def next(self):
-        # buy and hold
-        self.log("next")
+        txt = []
+        #txt.append('{}'.format(len(self)))
+        txt.append('{}'.format(self.data.datetime.datetime(0).isoformat()))
+        txt.append('{:.6f}'.format(self.data.open[0]))
+        txt.append('{:.6f}'.format(self.data.high[0]))
+        txt.append('{:.6f}'.format(self.data.low[0]))
+        txt.append('{:.6f}'.format(self.data.close[0]))
+        txt.append('{:.0f}'.format(self.data.volume[0]))
+        print(','.join(txt))
+
+        if not self.data_live:
+            return
+
+        if len(self) % 2 == 0:
+            print ('ORDER BUY Created')
+            self.buy(size = 3000)
+
+        if len(self) % 2 == 1:
+            print ('ORDER SELL Created')
+            self.sell(size = 3000)
+
